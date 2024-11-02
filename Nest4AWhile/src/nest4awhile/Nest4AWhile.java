892acfc1ce4296cc0af7df434b39c87b4c35f6d9
hgb <benjamin.haivan@gmail.com>
2024-11-02 10:26:46 -0400

    Java Add Version

--- Nest4AWhile/src/nest4awhile/Nest4AWhile.java
+++ Nest4AWhile/src/nest4awhile/Nest4AWhile.java
@@ -1,5 +1,8 @@
 package nest4awhile;
 
+import java.util.Random;
+import java.util.Scanner;
+
 /**
  *
  * @author Haivan Benjamin
@@ -11,7 +14,19 @@ public class Nest4AWhile
      */
     public static void main(String[] args) 
     {
+        Scanner scnr = new Scanner(System.in);
         
+        Random rand = new Random();
+        
+        int n = scnr.nextInt();
+        
+        for(int i = 1; i <= n; i++)
+        {
+            int a = rand.nextInt(0, 1001);
+            int b = rand.nextInt(0, 1001);
+            
+            System.out.println(a + b);
+        }
     }
 
 }
