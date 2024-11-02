16f6164b506060e61627cffb540a2135ae603c9b
TheAdaptoid <mhwkmn22@gmail.com>
2024-11-02 11:26:39 -0400

    moves

--- Smooth_Moves.py
+++ Smooth_Moves.py
@@ -3,5 +3,5 @@ for i in range(5):
     row: list[str] = input().split(" ")
     for j in range(5):
         if row[j] == "1":
-            value = abs(3 - i) + abs(3 - j)
+            value = abs(2 - i) + abs(2 - j)
 print(value)
